1076be08305b6dc0e10137ceae0fcd4f5b53ca70
Martin Schenck <martin.schenck@tu-berlin.de>
2012-09-25 19:08:33 +0200

    Refactored to create a linear list from a workflow

--- src/main/java/de/tuberlin/schenck/taverna_to_hadoop/convert/WorkflowManager.java
+++ src/main/java/de/tuberlin/schenck/taverna_to_hadoop/convert/WorkflowManager.java
@@ -2,21 +2,32 @@ package de.tuberlin.schenck.taverna_to_hadoop.convert;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 
+import uk.org.taverna.scufl2.api.common.NamedSet;
+import uk.org.taverna.scufl2.api.common.Scufl2Tools;
+import uk.org.taverna.scufl2.api.configurations.Configuration;
 import uk.org.taverna.scufl2.api.container.WorkflowBundle;
 import uk.org.taverna.scufl2.api.core.DataLink;
+import uk.org.taverna.scufl2.api.core.Processor;
 import uk.org.taverna.scufl2.api.io.ReaderException;
 import uk.org.taverna.scufl2.api.io.WorkflowBundleIO;
+import uk.org.taverna.scufl2.api.port.InputWorkflowPort;
+import uk.org.taverna.scufl2.api.port.OutputProcessorPort;
 import uk.org.taverna.scufl2.api.port.OutputWorkflowPort;
-import uk.org.taverna.scufl2.api.profiles.ProcessorBinding;
-import uk.org.taverna.scufl2.api.profiles.ProcessorInputPortBinding;
-import uk.org.taverna.scufl2.api.profiles.ProcessorOutputPortBinding;
-import de.tuberlin.schenck.taverna_to_hadoop.convert.activity_configs.IConfig;
+import uk.org.taverna.scufl2.api.port.ReceiverPort;
+import uk.org.taverna.scufl2.api.port.SenderPort;
+import de.tuberlin.schenck.taverna_to_hadoop.convert.activity_configs.ActivityConfig;
 import de.tuberlin.schenck.taverna_to_hadoop.exceptions.UnsupportedWorkflowException;
+import de.tuberlin.schenck.taverna_to_hadoop.utils.Config;
 
 public class WorkflowManager {
 	/** The logger for this class. */
@@ -54,42 +65,6 @@ public class WorkflowManager {
 		} catch (IOException e) {
 			logger.error("Could not read " + input, e);
 		}
-		
-		checkWorkflowSupport();
-	}
-
-	/**
-	 * Check whether the read workflow is supported.
-	 * <p>
-	 * Support depends on the version of this program.
-	 * Newer versions naturally support more workflows.
-	 * 
-	 * @throws UnsupportedWorkflowException the workflow is not supported (at the moment)
-	 */
-	private void checkWorkflowSupport() throws UnsupportedWorkflowException {
-		// Only one workflow supported at the moment
-		if(workflowBundle.getWorkflows().size() > 1)
-			throw new UnsupportedWorkflowException("Found more than one workflow in the bundle.");
-		
-		// Only one input port supported at the moment
-		if(workflowBundle.getMainWorkflow().getInputPorts().size() != 1)
-			throw new UnsupportedWorkflowException("Found more or fewer than one input port.");
-		
-		// Only one output port is supported at the moment
-		if(workflowBundle.getMainWorkflow().getOutputPorts().size() != 1)
-			throw new UnsupportedWorkflowException("Found more or fewer than one output port.");
-				
-		// Only beanshell activities are allowed at the moment
-		if(!workflowBundle.getMainProfile().getActivities().first().getConfigurableType().toString().equals("http://ns.taverna.org.uk/2010/activity/beanshell"))
-			throw new UnsupportedWorkflowException("Found activity other than beanshell: " + workflowBundle.getMainProfile().getActivities().first().getConfigurableType());
-		
-		// Only one input port for the beanshell allowed at the moment
-		if(workflowBundle.getMainProfile().getActivities().first().getInputPorts().size() != 1)
-			throw new UnsupportedWorkflowException("Found more or fewer than one input port for activity.");
-
-		// Only one output port for the beanshell allowed at the moment
-		if(workflowBundle.getMainProfile().getActivities().first().getOutputPorts().size() != 1)
-			throw new UnsupportedWorkflowException("Found more or fewer than one output port for activity.");
 	}
 	
 	/**
@@ -104,7 +79,8 @@ public class WorkflowManager {
 	 * @return the template
 	 */
 	public String createTemplateFromWorkflow(String inputTemplate) {
-		List<IConfig> activityList = createListFromWorkflow();
+		List<ActivityConfig> activityList = createListFromWorkflow();
+		logger.info("Resulting list: " + activityList);
 		return translate(inputTemplate, activityList);
 	}
 	
@@ -113,24 +89,86 @@ public class WorkflowManager {
 	 * Starts at output port and recursively goes backwards until input port is reached.
 	 * Then reverses the order.
 	 */
-	private List<IConfig> createListFromWorkflow() {
-		List<IConfig> result = new ArrayList<IConfig>();
-		for(DataLink dataLink : workflowBundle.getMainWorkflow().getDataLinks()) {
-			logger.debug(dataLink.getReceivesFrom() + "->" + dataLink.getSendsTo());
+	private List<ActivityConfig> createListFromWorkflow() {
+		logger.info("Converting workflow to list of hadoop jobs.");
+		List<ActivityConfig> result = new ArrayList<ActivityConfig>();
+		
+		Scufl2Tools scufl2tools = new Scufl2Tools();
+		NamedSet<OutputWorkflowPort> workflowOutputPorts = workflowBundle.getMainWorkflow().getOutputPorts();
+		if(workflowOutputPorts.size() != 1) {
+			String message = "Only one output port for workflow allowed at the moment";
+			logger.error(message, new UnsupportedWorkflowException(message));
 		}
-		for(ProcessorBinding binding : workflowBundle.getMainProfile().getProcessorBindings()) {
-			logger.debug(binding.getName());
-			for(ProcessorInputPortBinding inBinding : binding.getInputPortBindings()) {
-				logger.debug("\tinput: " + inBinding.getBoundActivityPort() + ", " + inBinding.getBoundProcessorPort());
-			}
-			for(ProcessorOutputPortBinding outBinding : binding.getOutputPortBindings()) {
-				logger.debug("\toutput: " + outBinding);
-			}
+		logger.debug("Nr. of workflow output ports: " + workflowOutputPorts.size());
+		
+		// Get all activities which are linked to the output ports
+		// Go through all ports, get all feeding activities
+		for(OutputWorkflowPort workflowOutPort : workflowOutputPorts) {
+			getPreviousProcessors(result, scufl2tools, workflowOutPort);
 		}
-		for(OutputWorkflowPort port : workflowBundle.getMainWorkflow().getOutputPorts()) {
-			
-		}
+		scufl2tools.datalinksTo(workflowOutputPorts.first());
+		
+		// Reverse, because we went through the workflow backwards
+		Collections.reverse(result);
 		return result;
+	}
+
+	private void getPreviousProcessors(List<ActivityConfig> result,
+			Scufl2Tools scufl2tools, ReceiverPort receiverPort) {
+		// Remember which one to go through next
+		Set<ReceiverPort> nextReceiverPorts = new HashSet<ReceiverPort>();
+		
+		// Go through all links
+		for(DataLink dataLink : scufl2tools.datalinksTo(receiverPort)) {
+			SenderPort senderPort = dataLink.getReceivesFrom();
+			// No processor if input port of workflow
+			if(senderPort instanceof InputWorkflowPort)
+				continue;
+			
+			OutputProcessorPort processorOutPort = (OutputProcessorPort) senderPort;
+			Processor processor = processorOutPort.getParent();
+			Configuration configuration = scufl2tools.configurationForActivityBoundToProcessor(processor, workflowBundle.getMainProfile());
+			
+			logger.debug("Activity configuration found: "+ configuration.getName() + " - " + configuration.getConfigurableType());
+			String[] activityParams = configuration.getConfigurableType().getPath().split("/");
+			
+			// Find classname for configuration
+			String className = activityParams[activityParams.length - 1];
+			// First letter uppercase
+			className = className.substring(0, 1).toUpperCase() + className.substring(1);
+			// Append "Config"
+			className += "Config";
+			
+			ActivityConfig activityConfig = null;
+			try {
+				logger.debug("Instanciating class: " + className);
+				
+				Class<?> classForName = Class.forName(Config.getActivityConfigsPackage() + className);
+				Constructor<?> constructor = classForName.getConstructor(String.class);
+				activityConfig = (ActivityConfig) constructor.newInstance(configuration.getName());
+			} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | 
+					NoSuchMethodException | SecurityException | IllegalArgumentException | 
+					InvocationTargetException e) {
+				logger.error("Could not instanciate class " + className + ".", e);
+			}
+			
+			List<ActivityConfig> copyOfResult = new ArrayList<ActivityConfig>(result.size());
+			copyOfResult.addAll(result);
+			// If it is already in the list, remove and append
+			for(int i = 0; i < copyOfResult.size(); i++) {
+				if(copyOfResult.get(i).equals(activityConfig)) {
+					result.remove(i);
+				}
+			}
+			result.add(activityConfig);
+			
+			nextReceiverPorts.addAll(processor.getInputPorts());
+		}
+		
+		// Next step in the workflow
+		for(ReceiverPort nextReceiverPort : nextReceiverPorts) {
+			getPreviousProcessors(result, scufl2tools, nextReceiverPort);
+		}
 	}
 	
 	/**
@@ -140,9 +178,10 @@ public class WorkflowManager {
 	 * @param configurations a list of configurations for MapReduce calls
 	 * @return the template with the placeholder replaced by file inclusions
 	 */
-	public String translate(String template, List<IConfig> configurations) {
+	public String translate(String template, List<ActivityConfig> configurations) {
 				
 		// TODO put map reduce classes into template
+		// FIXME might better suit a translator or a different name
 		
 		return template;
 	}
