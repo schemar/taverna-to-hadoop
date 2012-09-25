6c30afdac6d80ee3c36794c059e37803494afbed
Martin Schenck <martin.schenck@tu-berlin.de>
2012-09-25 15:30:34 +0200

    Introducing Configs for Activities (Implementing IConfig)

---
+++ src/main/java/de/tuberlin/schenck/taverna_to_hadoop/convert/activity_configs/BeanshellConfig.java
@@ -0,0 +1,5 @@
+package de.tuberlin.schenck.taverna_to_hadoop.convert.activity_configs;
+
+public class BeanshellConfig implements IConfig {
+
+}
---
+++ src/main/java/de/tuberlin/schenck/taverna_to_hadoop/convert/activity_configs/IConfig.java
@@ -0,0 +1,10 @@
+package de.tuberlin.schenck.taverna_to_hadoop.convert.activity_configs;
+/**
+ * Configurations interface for Taverna activities.
+ * 
+ * @author schenck
+ *
+ */
+public interface IConfig {
+	
+}
