c4f5bde85f4e011b61e4bd2dcd0dd4759f64e170
Martin Schenck <martin.schenck@tu-berlin.de>
2012-11-13 20:13:46 +0100

    now supporting multipel ports this fixes #16 as well as it fixes #5 because as it is now, map will emit key/value pairs in a way that the dot product is built on the reducer side

--- src/main/java/de/tuberlin/schenck/taverna_to_hadoop/convert/WorkflowManager.java
+++ src/main/java/de/tuberlin/schenck/taverna_to_hadoop/convert/WorkflowManager.java
@@ -33,13 +33,13 @@ import de.tuberlin.schenck.taverna_to_hadoop.utils.Config;
 public class WorkflowManager {
 	/** The logger for this class. */
 	private static Logger logger = Logger.getLogger(WorkflowManager.class);
-	
+
 	/** The workflow bundle read from disk. */
 	private WorkflowBundle workflowBundle;
-	
+
 	/** The activity list created from the workflow for the MapReduce class. */
 	List<ActivityConfig> activityList;
-	
+
 	/**
 	 * Create a manager and read the workflow from disk.
 	 * 
@@ -49,7 +49,7 @@ public class WorkflowManager {
 	public WorkflowManager(String input) throws UnsupportedWorkflowException {
 		readWorkflow(input);
 	}
-	
+
 	/**
 	 * Reads a Taverna workflow from disk.
 	 * Format can be either t2flow or scufl2.
@@ -69,11 +69,11 @@ public class WorkflowManager {
 		} catch (IOException e) {
 			logger.error("Could not read " + input, e);
 		}
-		
+
 		activityList = createListFromWorkflow();
 		logger.info("List from workflow: " + activityList);
 	}
-	
+
 	/**
 	 * Converts the workflow to a linear list of MapReduce {@link de.tuberlin.schenck.taverna_to_hadoop.convert.activity_configs.ActivityConfig}.
 	 * Starts at output port and recursively goes backwards until input port is reached.
@@ -82,18 +82,18 @@ public class WorkflowManager {
 	private List<ActivityConfig> createListFromWorkflow() {
 		logger.info("Converting workflow to list of hadoop jobs.");
 		List<ActivityConfig> result = new ArrayList<ActivityConfig>();
-		
+
 		Scufl2Tools scufl2tools = new Scufl2Tools();
 		NamedSet<OutputWorkflowPort> workflowOutputPorts = workflowBundle.getMainWorkflow().getOutputPorts();
 
 		logger.debug("Nr. of workflow output ports: " + workflowOutputPorts.size());
-		
+
 		// Get all activities which are linked to the output ports
 		// Go through all ports, get all feeding activities
 		for(OutputWorkflowPort workflowOutPort : workflowOutputPorts) {
 			getPreviousProcessors(result, scufl2tools, workflowOutPort);
 		}
-		
+
 		// Reverse, because we went through the workflow backwards
 		Collections.reverse(result);
 		return result;
@@ -112,46 +112,61 @@ public class WorkflowManager {
 			Scufl2Tools scufl2tools, ReceiverPort receiverPort) {
 		// Remember which one to go through next
 		Set<ReceiverPort> nextReceiverPorts = new HashSet<ReceiverPort>();
-		
+
 		// Go through all links
 		for(DataLink dataLink : scufl2tools.datalinksTo(receiverPort)) {
 			SenderPort senderPort = dataLink.getReceivesFrom();
 			// No processor if input port of workflow
 			if(senderPort instanceof InputWorkflowPort)
 				continue;
-			
+
 			OutputProcessorPort processorOutPort = (OutputProcessorPort) senderPort;
 			Processor processor = processorOutPort.getParent();
 			Configuration configuration = scufl2tools.configurationForActivityBoundToProcessor(processor, workflowBundle.getMainProfile());
-			
+
 			logger.debug("Activity configuration found: "+ configuration.getName() + " - " + configuration.getConfigurableType());
 			String[] activityParams = configuration.getConfigurableType().getPath().split("/");
-			
+
 			// Find classname for configuration
 			String className = activityParams[activityParams.length - 1];
 			// First letter uppercase
 			className = className.substring(0, 1).toUpperCase() + className.substring(1);
 			// Append "Config"
 			className += "Config";
-			
+
 			ActivityConfig activityConfig = null;
 			try {
 				logger.debug("Instanciating class: " + className);
-				
+
 				// Load class from name
 				Class<?> classForName = Class.forName(Config.getActivityConfigsPackage() + className);
 				Constructor<?> constructor = classForName.getConstructor(String.class);
 				activityConfig = (ActivityConfig) constructor.newInstance(configuration.getName());
-				
+
 				// Transfer data from Taverna
 				activityConfig.fetchActivitySpecificDataFromTavernaConfig(configuration);
-				
+
 				// Add all ports
 				List<String> outputPorts = new ArrayList<String>(processor.getOutputPorts().size());
 				List<String> inputPorts = new ArrayList<String>(processor.getInputPorts().size());
-				
+
 				for(OutputProcessorPort outputProcessorPort : processor.getOutputPorts()) {
 					outputPorts.add(outputProcessorPort.getName());
+					
+					// Get mapping from input ports to output ports of previous processors
+					for(DataLink dataLinkToNext : scufl2tools.datalinksFrom(outputProcessorPort)) {
+						String outname = "";
+						if(dataLinkToNext.getSendsTo() instanceof InputProcessorPort) {
+							InputProcessorPort inPort = (InputProcessorPort) dataLinkToNext.getSendsTo();
+							outname = inPort.getParent().getName() + "_" + dataLinkToNext.getSendsTo().getName();
+						} else if(dataLinkToNext.getSendsTo() instanceof OutputWorkflowPort) {
+							outname = dataLinkToNext.getSendsTo().getName();
+						} else {
+							throw new UnsupportedWorkflowException("Unknown port type.");
+						}
+						
+						activityConfig.addToPortMap(outputProcessorPort.getName(), outname);
+					}
 				}
 				for(InputProcessorPort inputProcessorPort : processor.getInputPorts()) {
 					inputPorts.add(inputProcessorPort.getName());
@@ -180,8 +195,10 @@ public class WorkflowManager {
 			} catch (InvocationTargetException e) {
 				logger.error("Unsupported workflow.", new UnsupportedWorkflowException());
 				logger.error("Could not instanciate class " + className + ".", e);
+			} catch (UnsupportedWorkflowException e) {
+				logger.error("Could not translate workflow.", e);
 			}
-			
+
 			List<ActivityConfig> copyOfResult = new ArrayList<ActivityConfig>(result.size());
 			copyOfResult.addAll(result);
 			// If it is already in the list, remove and append
@@ -191,16 +208,16 @@ public class WorkflowManager {
 				}
 			}
 			result.add(activityConfig);
-			
+
 			nextReceiverPorts.addAll(processor.getInputPorts());
 		}
-		
+
 		// Next step in the workflow to reach all
 		for(ReceiverPort nextReceiverPort : nextReceiverPorts) {
 			getPreviousProcessors(result, scufl2tools, nextReceiverPort);
 		}
 	}
-	
+
 	/**
 	 * Get the String for the template for all map and reduce classes from the workflow.
 	 *  
@@ -208,17 +225,17 @@ public class WorkflowManager {
 	 */
 	public String getMapReduceClasses() {
 		StringBuilder resultBuilder = new StringBuilder();
-		
+
 		for(ActivityConfig activityConfig : activityList) {
 			resultBuilder.append(activityConfig.getMapReduce());
 			resultBuilder.append("\n");
 		}
-		
+
 		String result = resultBuilder.toString();
 		logger.debug("Map and reduce classes: " + result);
 		return result;
 	}
-	
+
 	/**
 	 * Get the String for the template for all run methods from the workflow.
 	 *  
@@ -226,24 +243,36 @@ public class WorkflowManager {
 	 */
 	public String getRuns() {
 		StringBuilder resultBuilder = new StringBuilder();
-		
-		String inputPath = "args[0]";
+
+		String inputPath = "";
 		String intermediatePath;
 		for(ActivityConfig activityConfig : activityList) {
-			intermediatePath = "\"" + activityConfig.getName() + "_" + activityConfig.getOutputPort() + "\"";
-					
+			// TODO what if input from args
+			StringBuilder pathBuilder = new StringBuilder();
+			pathBuilder.append("\"");
+			for(String inputPort : activityConfig.getInputPorts()) {
+				pathBuilder.append(activityConfig.getName());
+				pathBuilder.append("_");
+				pathBuilder.append(inputPort);
+				pathBuilder.append(",");
+			}
+			
+			pathBuilder.deleteCharAt(pathBuilder.length() - 1);
+			pathBuilder.append("\"");
+			inputPath = pathBuilder.toString();
+			
+			intermediatePath = "\"" + activityConfig.getOutputToNextInput().get(activityConfig.getOutputPorts().get(0)) + "\"";
+
 			activityConfig.setInputPath(inputPath);
 			activityConfig.setOutputPath(intermediatePath);
-			
+
 			activityConfig.setInputFormat("TextInputFormat");
 			activityConfig.setOutputFormat("TextOutputFormat");
-			
+
 			resultBuilder.append(activityConfig.getRun());
 			resultBuilder.append("\n");
-			
-			inputPath = intermediatePath;
 		}
-		
+
 		return resultBuilder.toString();
 	}
 }
